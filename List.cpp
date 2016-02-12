a716e2d5223b005d8c86aa37c033ae7248531d0f
montellese <montellese@xbmc.org>
2016-02-12 18:07:43 +0100

    [android] CJNIList: make get() and size() const

--- List.cpp
+++ List.cpp
@@ -30,7 +30,7 @@
 using namespace jni;
 
 template <typename T>
-T CJNIList<T>::get(int index)
+T CJNIList<T>::get(int index) const
 {
   return (T)call_method<jhobject>(m_object,
     "get", "(I)Ljava/lang/Object;",
@@ -38,7 +38,7 @@ T CJNIList<T>::get(int index)
 }
 
 template <typename T>
-int CJNIList<T>::size()
+int CJNIList<T>::size() const
 {
   return m_object.get() ? call_method<jint>(m_object,
     "size", "()I") : 0;
