a605a5d07ba3f1e823d02a1185f70a65dd130abc
Chris "Koying" Browet <cbro@semperpax.com>
2016-12-29 19:34:00 +0100

    Initial IBinder & friends

---
+++ IBinder.cpp
@@ -0,0 +1,95 @@
+/*
+ *      Copyright (C) 2016 Christian Browet
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "IBinder.h"
+#include "jutils/jutils-details.hpp"
+
+#include "IInterface.h"
+#include "FileDescriptor.h"
+#include "Parcel.h"
+
+using namespace jni;
+
+
+void CJNIIBinderDeathRecipient::binderDied()
+{
+}
+
+/*********************/
+
+void CJNIIBinder::dump(const CJNIFileDescriptor& fd, const std::vector<std::string>& args)
+{
+  call_method<void>(m_object,
+                    "dump", "(Ljava/io/FileDescriptor;[Ljava/lang/String;)V",
+                    fd.get_raw(), jcast<jhobjectArray>(args));
+}
+
+void CJNIIBinder::dumpAsync(const CJNIFileDescriptor& fd, const std::vector<std::string>& args)
+{
+  call_method<void>(m_object,
+                    "dumpAsync", "(Ljava/io/FileDescriptor;[Ljava/lang/String;)V",
+                    fd.get_raw(), jcast<jhobjectArray>(args));
+}
+
+std::string CJNIIBinder::getInterfaceDescriptor()
+{
+  return jcast<std::string>(call_method<jhstring>(m_object,
+                                                  "getInterfaceDescriptor", "()Ljava/lang/String;"));
+}
+
+bool CJNIIBinder::isBinderAlive()
+{
+  return call_method<jboolean>(m_object,
+                               "isBinderAlive", "()Z");
+}
+
+void CJNIIBinder::linkToDeath(const CJNIIBinderDeathRecipient& recipient, int flags)
+{
+  call_method<void>(m_object,
+                    "linkToDeath", "(Landroid/os/IBinder$DeathRecipient;I)V",
+                    recipient.get_raw(), flags);
+}
+
+bool CJNIIBinder::pingBinder()
+{
+  return call_method<jboolean>(m_object,
+                               "pingBinder", "()Z");
+}
+
+CJNIIInterface CJNIIBinder::queryLocalInterface(const std::string& descriptor)
+{
+  return call_method<jhobject>(m_object,
+                               "queryLocalInterface", "(Ljava/lang/String;)Landroid/os/IInterface;",
+                               jcast<jhstring>(descriptor));
+}
+
+bool CJNIIBinder::transact(int code, const CJNIParcel& data, const CJNIParcel& reply, int flags)
+{
+  return call_method<jboolean>(m_object,
+                               "transact", "(ILandroid/os/Parcel;Landroid/os/Parcel;I)Z",
+                               code, data.get_raw(), reply.get_raw(), flags);
+}
+
+bool CJNIIBinder::unlinkToDeath(const CJNIIBinderDeathRecipient& recipient, int flags)
+{
+  return call_method<jboolean>(m_object,
+                               "unlinkToDeath", "(Landroid/os/IBinder$DeathRecipient;I)Z",
+                               recipient.get_raw(), flags);
+}
---
+++ IBinder.h
@@ -0,0 +1,57 @@
+#pragma once
+/*
+ *      Copyright (C) 2016 Christian Browet
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "JNIBase.h"
+
+namespace jni
+{
+
+class CJNIIInterface;
+class CJNIParcel;
+class CJNIFileDescriptor;
+
+class CJNIIBinderDeathRecipient : public CJNIBase
+{
+public:
+  virtual void binderDied();
+};
+
+class CJNIIBinder : public CJNIBase
+{
+public:
+  CJNIIBinder(const jni::jhobject &object) : CJNIBase(object) {}
+  virtual ~CJNIIBinder() {}
+
+  void dump(const CJNIFileDescriptor& fd, const std::vector<std::string>& args);
+  void dumpAsync(const CJNIFileDescriptor& fd, const std::vector<std::string>& args);
+  std::string getInterfaceDescriptor();
+  bool isBinderAlive();
+  void linkToDeath(const CJNIIBinderDeathRecipient& recipient, int flags);
+  bool pingBinder();
+  CJNIIInterface queryLocalInterface(const std::string& descriptor);
+  bool transact(int code, const CJNIParcel& data, const CJNIParcel& reply, int flags);
+  bool unlinkToDeath(const CJNIIBinderDeathRecipient& recipient, int flags);
+
+protected:
+  CJNIIBinder();
+};
+
+}
