2e720860a9a65811fcbba91720f0f447efbb3feb
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2022-05-19 13:42:01 +0200

    Add CJNICanvas. Extend CJNIDrawable, CJNIBitmap.

--- src/Canvas.cpp
+++ src/Canvas.cpp
@@ -1,7 +1,6 @@
-#pragma once
 /*
- *      Copyright (C) 2013 Team XBMC
- *      http://xbmc.org
+ *      Copyright (C) 2022 Team Kodi
+ *      http://kodi.tv
  *
  *  This Program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -19,25 +18,26 @@
  *
  */
 
-#include "JNIBase.h"
+#include "Canvas.h"
 
-class CJNIBitmap : public CJNIBase
+#include "Bitmap.h"
+#include "jutils-details.hpp"
+
+using namespace jni;
+
+static std::string s_className = "android/graphics/Canvas";
+
+CJNICanvas::CJNICanvas(const CJNIBitmap& bitmap) : CJNIBase(s_className)
 {
-public:
-  CJNIBitmap() : CJNIBase() {}
-  CJNIBitmap(const jni::jhobject &object) : CJNIBase(object) {}
-  ~CJNIBitmap() {}
+  m_object = new_object(s_className, "<init>", "(Landroid/graphics/Bitmap;)V", bitmap.get_raw());
+}
 
-  enum Config
-  {
-    ALPHA_8,
-    ARGB_4444,
-    ARGB_8888,
-    HARDWARE,
-    RGBA_1010102,
-    RGBA_F16,
-    RGB_565,
-  };
+int CJNICanvas::getWidth()
+{
+  return call_method<int>(m_object, "getWidth", "()I");
+}
 
-  static CJNIBitmap createBitmap(int width, int height, CJNIBitmap::Config config);
-};
+int CJNICanvas::getHeight()
+{
+  return call_method<int>(m_object, "getHeight", "()I");
+}
